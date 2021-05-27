c6348632beec98ef50a51e61185a8c4816260d2e
wahyu-aji <wahyuaji16120@gmail.com>
2021-05-28 06:58:01 +0700

    ubah database

--- application/config/database.php
+++ application/config/database.php
@@ -81,7 +81,7 @@ $db['default'] = array(
     'database' => 'db_tugas',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
-    'pconnect' => FALSE,
+    'pconnect' => TRUE/FALSE,
     'db_debug' => (ENVIRONMENT !== 'production'),
     'cache_on' => FALSE,
     'cachedir' => '',
