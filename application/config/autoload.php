4b074b6549abc424645e9504983a7ba3a9b633eb
wahyu-aji <wahyuaji16120@gmail.com>
2021-05-27 18:46:27 +0700

    ubah autoload libraries

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','some class');
+$autoload['libraries'] = array('database');
 
 /*
 | -------------------------------------------------------------------
